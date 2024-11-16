b1089f7ba85031ac89f884e67a0d6122f2e772b0
pongpatapee <pongpatapee1021@gmail.com>
2024-11-16 18:38:24 -0500

    feat: admiror product section

--- app/[country_code]/product/admiror/page.tsx
+++ app/[country_code]/product/admiror/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex flex-col">
@@ -15,23 +13,24 @@ export default function Home() {
       </section>
 
       {/* product-admiror-computer */}
-      <section className="text-white" style={{ backgroundColor: "#000" }}>
+      <section className="text-white bg-black p-10">
         <div className="flex flex-col justify-center items-center">
-          <h1 className="text-5xl  ">#YouCanBe Expressing Yourself</h1>
-          <div className="text-md  ">
+          <h1 className="text-5xl m-5">#YouCanBe Expressing Yourself</h1>
+          <p className="text-lg m-10">
             Be who you are and show your individuality. The choice is yours.
-            #YouCanbe{" "}
-          </div>
+            #YouCanbe
+          </p>
           <div className="Fadein" style={{ opacity: 0.597166 }}>
             <img src="/images/admiror/pic_AMIROR01.png" />
           </div>
-          <div className="text-md  max-w-2xl">
+          <p className="text-lg  max-w-3xl leading-8 m-5">
             The all-new AVITA ADMIROR takes you on a journey of self-discovery.
             Shattering the norms and constraints of conventional laptops, the
             ADMIROR’s intricate designs and eye-catching colours are yours to
             embrace, because you deserve to be extraordinary!
-          </div>
-          <div className="banner-data text-center col-lg-12">
+          </p>
+
+          <div className="m-20">
             <img
               className="hidden-sm-down"
               src="/images/admiror/pic_feature01_15.png"
@@ -41,50 +40,48 @@ export default function Home() {
             {/*   src="/images/admiror/pic_feature01_15_mob.png" */}
             {/* /> */}
           </div>
-          <h1 className="text-5xl">AVITA ADMIROR brings you</h1>
 
-          {/* section redirect buttons */}
+          {/* banner anchors */}
+          <h1 className="text-5xl mb-20">AVITA ADMIROR brings you</h1>
           <section>
-            <div className="">
-              <div className="row">
-                <div className="col-md-4">
-                  <a href="#artistic_design">
-                    <img
-                      className="first"
-                      src="/images/admiror/btn_artistic_1.png"
-                    />
-                    {/* <img */}
-                    {/*   className="hover" */}
-                    {/*   src="/images/admiror/btn_artistic_2.png" */}
-                    {/* /> */}
-                  </a>
-                </div>
+            <div className="flex flex-row max-w-6xl">
+              <div className="mx-5 max-w-[33%]">
+                <a href="#artistic_design">
+                  <img
+                    className="first"
+                    src="/images/admiror/btn_artistic_1.png"
+                  />
+                  {/* <img */}
+                  {/*   className="hover" */}
+                  {/*   src="/images/admiror/btn_artistic_2.png" */}
+                  {/* /> */}
+                </a>
+              </div>
 
-                <div className="col-md-4">
-                  <a href="#usage_experience">
-                    <img
-                      className="first"
-                      src="/images/admiror/btn_pride_1.png"
-                    />
-                    {/* <img */}
-                    {/*   className="hover" */}
-                    {/*   src="/images/admiror/btn_pride_2.png" */}
-                    {/* /> */}
-                  </a>
-                </div>
+              <div className="mx-5 max-w-[33%]">
+                <a href="#usage_experience">
+                  <img
+                    className="first"
+                    src="/images/admiror/btn_pride_1.png"
+                  />
+                  {/* <img */}
+                  {/*   className="hover" */}
+                  {/*   src="/images/admiror/btn_pride_2.png" */}
+                  {/* /> */}
+                </a>
+              </div>
 
-                <div className="col-md-4">
-                  <a href="#outstanding">
-                    <img
-                      className="first"
-                      src="/images/admiror/btn_outstanding_1.png"
-                    />
-                    {/* <img */}
-                    {/*   className="hover" */}
-                    {/*   src="/images/admiror/btn_outstanding_2.png" */}
-                    {/* /> */}
-                  </a>
-                </div>
+              <div className="mx-5 max-w-[33%]">
+                <a href="#outstanding">
+                  <img
+                    className="first"
+                    src="/images/admiror/btn_outstanding_1.png"
+                  />
+                  {/* <img */}
+                  {/*   className="hover" */}
+                  {/*   src="/images/admiror/btn_outstanding_2.png" */}
+                  {/* /> */}
+                </a>
               </div>
             </div>
           </section>
