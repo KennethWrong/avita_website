b79ba0156b5d9cf961a37fc018818a4f9453182d
pongpatapee <pongpatapee1021@gmail.com>
2024-11-16 21:40:25 -0500

    feat: extra laptop desc section

--- app/[country_code]/product/admiror/page.tsx
+++ app/[country_code]/product/admiror/page.tsx
@@ -152,6 +152,44 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      {/* extra laptop description */}
+      <section className="bg-white flex flex-col justify-center items-center p-36 min-h-96">
+        <img className="mb-5" src="/images/admiror/ADMIROR02_red.png" />
+        <div className="flex flex-row max-w-6xl">
+          <div className="mr-24">
+            <div
+              className="text-[#7accc8] text-2xl leading-8 tracking-[0.1em]
+                    font-light uppercase text-left pb-5 mt-16"
+            >
+              Ultra-compact body for unsurpassed portability
+            </div>
+
+            <span className="leading-8">
+              The ultra-portable ADMIROR is housed in a light metallic chassis.
+              At merely 9mm at its thinnest part, 14” and 15.6” weighs from just
+              1.32kg and 1.79kg respectively, making it extremely handy to carry
+              around as your favourite gadget to roam the world over.
+            </span>
+          </div>
+
+          <div>
+            <div
+              className="text-[#7accc8] text-2xl leading-8 tracking-[0.1em]
+                  font-light uppercase text-left pb-5 mt-16"
+            >
+              Push your limits with a wider view
+            </div>
+
+            <span className="leading-8">
+              Push the limits by garnering a wider view. The 7mm narrow bezel on
+              the ADMIROR gives your maximised screen areas and eliminates
+              distraction for an immersive viewing experience, whether you are
+              at work or just want to sit back and chill.
+            </span>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
