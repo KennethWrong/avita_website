4247deb1de7815ad31e9c26233bbd3b3c5d67f4b
pongpatapee <pongpatapee1021@gmail.com>
2024-11-17 23:16:22 -0500

    refactor: changed pura image dir name to match original site
    more convnient when copying elements

--- app/[country_code]/product/pura-a+/page.tsx
+++ app/[country_code]/product/pura-a+/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main>
       <ProductBanner
-        desktopImage={"/images/pura_a/AVITA_pura-a-plus-pc_en.jpg"}
-        mobileImage={"/images/pura_a/AVITA_pura-a-plus-mo_en.jpg"}
+        desktopImage={"/images/pura-a-plus/AVITA_pura-a-plus-pc_en.jpg"}
+        mobileImage={"/images/pura-a-plus/AVITA_pura-a-plus-mo_en.jpg"}
         altText={"Pura A+ Banner"}
       />
     </main>
